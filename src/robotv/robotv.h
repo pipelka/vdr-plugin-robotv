eebafcd7dc705eb9b4f0ba6f8f822251398cd941
Alexander Pipelka <alexander.pipelka@gmail.com>
2016-04-03 20:31:43 +0200

    Version 0.9.99

--- src/robotv/robotv.h
+++ src/robotv/robotv.h
@@ -28,7 +28,7 @@
 
 #include "robotvserver.h"
 
-static const char* VERSION        = "0.9.98";
+static const char* VERSION        = "0.9.99";
 static const char* DESCRIPTION    = "roboTV Server";
 
 class PluginRoboTVServer : public cPlugin {
