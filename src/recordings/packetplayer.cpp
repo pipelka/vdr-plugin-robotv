aca95af416fa64dd9f047137de60f784b3e5c620
Alexander Pipelka <alexander.pipelka@gmail.com>
2017-01-12 18:58:21 +0100

    [PacketPlayer] ignore packets until demuxers are ready

--- src/recordings/packetplayer.cpp
+++ src/recordings/packetplayer.cpp
@@ -49,6 +49,10 @@ PacketPlayer::~PacketPlayer() {
 }
 
 void PacketPlayer::sendStreamPacket(StreamPacket* p) {
+    if(!m_demuxers.isReady() || m_requestStreamChange) {
+        return;
+    }
+
     // check if we've got a key frame
     if(p->content == StreamInfo::scVIDEO && p->frameType == StreamInfo::ftIFRAME && !m_firstKeyFrameSeen) {
         isyslog("got first key frame");
