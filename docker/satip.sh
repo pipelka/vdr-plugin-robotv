cd4d0a6562ac17eb1d75be75567cc2de394c1bac
Alexander Pipelka <alexander.pipelka@gmail.com>
2021-03-01 08:25:44 +0100

    [docker] updated scripts

--- docker/satip.sh
+++ docker/satip.sh
@@ -24,15 +24,21 @@ docker run \
 	-e STREAMDEV_CLIENT_ENABLE=0 \
 	-e VDR_LOGLEVEL=3 \
 	-e VDR_UPDATECHANNELS=3 \
-	-e VDR_EPGSCANTYPE=2 \
+	-e VDR_EPGSCANTYPE=1 \
 	-v $PWD/mounts/data:/data \
 	-v $PWD/mounts/cache:/data/cache \
 	-v $PWD/mounts/video:/video \
 	-v $PWD/mounts/timeshift:/timeshift \
 	-e SATIP_ENABLE=1 \
-	-e SATIP_SERVER="192.168.16.2" \
+	-e SATIP_SERVER="192.168.16.2|DVBS2-8|Kathrein:0x41" \
 	-e SATIP_NUMDEVICES=2 \
-	--net=host \
+	-e SATIP_TRANSPORTMODE=0 \
+	-e SATIP_PORTRANGE="23000-23007" \
+	-e SATIP_ENABLEFRONTENDREUSE=0 \
+	-p 3000:3000 \
+	-p 2004:2004 \
+	-p 6419:6419 \
 	-p 34892:34892 \
+	-p 23000-23007:23000-23007/udp \
 	pipelka/robotv:latest
 
